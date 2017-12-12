f65ef7566bc468931bef76907b4bb361256dc3a5
rober101 <rober.isr@hotmail.com>
2017-12-12 13:20:07 -0600

    diseño  ya funciona pero todabia se ve mal

--- app/src/main/java/com/example/proyecto/myapplication/MainActivity.java
+++ app/src/main/java/com/example/proyecto/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.proyecto.myapplication;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+
+import java.security.MessageDigest;
+import java.security.Signature;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
